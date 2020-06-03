9c8bc46fd6ca0eee6df56e9c871a4332f622c252
Matheus Sunderhus <matheus.sunderhus@interprocess.com.br>
2020-06-03 14:28:41 -0300

    - Adiciona total de transações ao  response header - Abilita exposição de headers customizados na resposta.

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
 import 'reflect-metadata';
 import 'dotenv/config';
 
-import express, { Request, Response, NextFunction } from 'express';
+import express, { Response, Request, NextFunction } from 'express';
 import cors from 'cors';
 import 'express-async-errors';
 
@@ -12,7 +12,11 @@ import createConnection from './database';
 
 createConnection();
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    exposedHeaders: ['X-Total-Count'],
+  }),
+);
 
 app.use(express.json());
 app.use(routes);
